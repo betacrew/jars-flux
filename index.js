0751faac4e034753b6b52fde8d3a9168be9a49f0
Ronil M <ronil@bitbucket.io>
2022-05-03 13:58:10 +0530

    Add flux lib to npm repo

---
+++ index.js
@@ -0,0 +1,7 @@
+import useFlexCleanup from './libs/FlexCleanup'
+import store from './libs/store'
+
+module.exports = {
+    useFlexCleanup,
+    store
+}
---
+++ libs/sagamanager.js
@@ -0,0 +1,19 @@
+export default function createSagaManager(sagaMiddleware) {
+    let sagas = [];
+    let emitSagaAdditionListener = null;
+
+    return {
+        addSaga: function(name, saga) {
+            if (sagas[name]) {
+                return;
+            }
+            sagas = { ...sagas, [name]: saga };
+            if (emitSagaAdditionListener) {
+                emitSagaAdditionListener(saga);
+            }
+        },
+        setAddSagaListener: function(listener) {
+            emitSagaAdditionListener = listener;
+        }
+    }
+}
